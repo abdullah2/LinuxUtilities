6b215b69e041042484780bef78a9db6903d01600
Muhamed <muhamed.huseinbasic@outlook.com>
2017-01-09 07:04:06 +0100

    building store configuration functionallity

--- app/src/main/java/com/example/demahum/linuxutilities/store_configuration.java
+++ app/src/main/java/com/example/demahum/linuxutilities/store_configuration.java
@@ -1,11 +1,15 @@
 package com.example.demahum.linuxutilities;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class store_configuration extends AppCompatActivity {
 
@@ -15,15 +19,42 @@ public class store_configuration extends AppCompatActivity {
         setContentView(R.layout.activity_store_configuration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Store new configuration");
+        final SQLiteDatabase db = openOrCreateDatabase("conf", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS hosts (name text, host text, port text, username text, password text, primary key(name));");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        final EditText confName = (EditText) (findViewById(R.id.confName));
+        final EditText hostIP = (EditText) (findViewById(R.id.host_ip));
+        final EditText port = (EditText) (findViewById(R.id.port));
+        final EditText username = (EditText) (findViewById(R.id.username));
+        final EditText password = (EditText) (findViewById(R.id.password));
+        final Button saveButton = (Button) (findViewById(R.id.saveButton));
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String confName_string = confName.getText().toString();
+                String host_string = hostIP.getText().toString();
+                String port_string = port.getText().toString();
+                String username_string = username.getText().toString();
+                String password_string = password.getText().toString();
+
+                SQLiteStatement stmt = db.compileStatement("INSERT INTO hosts values (?, ?, ?, ?, ?)");
+                stmt.bindString(1, confName_string);
+                stmt.bindString(2, host_string);
+                stmt.bindString(3, port_string);
+                stmt.bindString(4, username_string);
+                stmt.bindString(5, password_string);
+                try{
+                    stmt.execute();
+                }catch (android.database.sqlite.SQLiteConstraintException e){
+                    Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Configuration name already exists!", Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }
             }
         });
     }
-
 }
